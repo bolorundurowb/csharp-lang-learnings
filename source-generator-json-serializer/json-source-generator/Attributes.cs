d33f42d541fdd14b95cd0c4dbf0d15b93c43bd04
Winner-Timothy Bolorunduro <bolorundurowb@gmail.com>
2025-07-21 14:11:15 +0100

    ft: remove the old source generator logic

--- source-generator-json-serializer/json-source-generator/Attributes.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace JsonSourceGenerator;
-
-[AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
-public class JsonSerializableAttribute : Attribute { }
-
-[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
-public class JsonIgnoreFieldAttribute : Attribute { }
