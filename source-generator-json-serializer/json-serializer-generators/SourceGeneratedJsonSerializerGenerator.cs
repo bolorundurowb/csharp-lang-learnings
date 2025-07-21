359e798987e0a0b850c46f6a2866394c6f8abc4c
Winner-Timothy Bolorunduro <bolorundurowb@gmail.com>
2025-07-21 14:11:35 +0100

    ft: add a new generator project

---
+++ source-generator-json-serializer/json-serializer-generators/SourceGeneratedJsonSerializerGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+
+namespace JsonSerializerGenerators;
+
+public class SourceGeneratedJsonSerializerGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        context.RegisterPostInitializationOutput(ctx =>
+        {
+            ctx.AddSource("SourceJsonSerializableAttribute.g.cs", SourceGenerationHelpers.ObjectLevelAttribute);
+            ctx.AddSource("SourceJsonIgnoreFieldAttribute.g.cs", SourceGenerationHelpers.FieldLevelAttribute);
+        });
+    }
+}
---
+++ source-generator-json-serializer/json-serializer-generators/SourceGenerationHelpers.cs
@@ -0,0 +1,16 @@
+﻿namespace JsonSerializerGenerators;
+
+public static class SourceGenerationHelpers
+{
+    public const string ObjectLevelAttribute = """
+                                               namespace JsonSerializerGenerators;
+                                               [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+                                               public class SourceJsonSerializableAttribute : Attribute { }
+                                               """;
+
+    public const string FieldLevelAttribute = """
+                                              namespace JsonSerializerGenerators;
+                                              [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
+                                              public class SourceJsonIgnoreFieldAttribute : Attribute { }
+                                              """;
+}
